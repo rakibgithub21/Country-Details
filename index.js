f0f02ca7a513b967d9dbad8f16025eaeafea328d
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-02-27 21:18:11 +0600

    single country details added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const loadCountries = async () => {
 const displayCountries = (countries) => {
     const divContainer = document.getElementById('div-container');
     countries.forEach(country => {
-        console.log(country);
+        // console.log(country);
         const div = document.createElement('div');
         // div.classList.add('card, bg-primary text-primary-content');
         div.classList.add('card')
@@ -20,13 +20,51 @@ const displayCountries = (countries) => {
             <h2 class="card-title">Indipendent: ${country.independent}</h2>
             <h2 class="card-title">Capital: ${country.capital}</h2>
             <div>
-            <button class="btn btn-outline btn-secondary">Show Details</button>
+            <button onclick = "loadCountryInfo('${country.cca2}')" class="btn btn-outline btn-secondary">Show Details</button>
             </div>
             
          </div>
         `;
         divContainer.appendChild(div)
     });
+}
+
+
+const loadCountryInfo = async (code) => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    console.log(url);
+    const res = await fetch(url);
+    const data = await res.json();
+    displayInfo(data[0])
+}
+
+const displayInfo = (country) => {
+    console.log(country);
+    const singleCountryContainer = document.getElementById('singleCountryContainer');
+    singleCountryContainer.innerHTML = ``
+    const div = document.createElement('div');
+
+    // class="card w-96 bg-base-100 shadow-xl image-full";
+    div.classList.add('my-10')
+    div.classList.add('card')
+    div.classList.add('lg:w-5/12');
+    div.classList.add('bg-base-100')
+    div.classList.add('shadow-xl')
+    div.classList.add('image-full');
+    div.classList.add('mx-auto')
+    div.innerHTML = `
+    <figure><img class="w-full" src="${country.flags.png}" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">${country.name?.official}</h2>
+                        <p class="font-bold text-rose-600">Flags Details: <span class = "font-semibold text-purple-200">${country.flags?.alt || 'No information available'}</span></p>
+                        <p>Population: ${country.population}</p>
+                        <p>Start of Week: ${country.startOfWeek}</p>
+                        <p>Region: ${country.region}</p>
+                        <p>Sub Region: ${country.subregion}</p>
+                        
+                    </div>
+    `;
+    singleCountryContainer.appendChild(div)
 }
 
 
